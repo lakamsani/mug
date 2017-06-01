ed190863ce96e9b2a73ac3d666c9589585f77949
Vamsee Lakamsani <vamsee@onkore.com>
2017-06-01 13:31:44 -0700

    added commands to connect with password embedded url directly.

--- scripts/amongo.sh
+++ scripts/amongo.sh
@@ -13,3 +13,9 @@ MONGO_PASS=`credstash get mug.admin.pass`
 #USER=mugapp
 #MONGO_PASS=`credstash get mug.mongo.pass`
 $HOME/atlas/bin/mongo  mongodb://mug-shard-00-00-ssbsr.mongodb.net:27017,mug-shard-00-01-ssbsr.mongodb.net:27017,mug-shard-00-02-ssbsr.mongodb.net:27017/mug?replicaSet=MUG-shard-0    --ssl --username $USER  -p $MONGO_PASS  --authenticationDatabase admin 
+
+
+# Another way is to simply supply the Atlas URL with the password in it.
+#MONGO_URL=`credstash get mug.mongo.url`
+#$HOME/atlas/bin/mongo $MONGO_URL
+
