396f3bbd85e6cd26e243c6e1e8b6ebb6b9eae651
Vamsee Lakamsani <vamsee@onkore.com>
2017-06-04 17:29:51 -0700

    logging updates.

--- controllers/credential.js
+++ controllers/credential.js
@@ -5,8 +5,6 @@ var log = log4js.getLogger('credential');
 let Credstash = require('nodecredstash');
 var config = require('config');
 
-process.env.AWS_DEFAULT_REGION="us-west-2"
-process.env.AWS_PROFILE="mug"
 
 let credstash = new Credstash({awsOpts: {region: process.env.AWS_DEFAULT_REGION}})
 
@@ -21,10 +19,10 @@ async function initCreds() {
   let credConfig = config.get("credentials")
 
   for (const [key, value] of Object.entries(credConfig)) {
-    log.info(`${key}: ${value}`);
+    //log.info(`${key}: ${value}`);
     const pass = await credstash.getSecret({name: value})
     process.env[key] = pass
-    log.info(`${process.env[key]}`);
+    //log.info(`${process.env[key]}`);
   }
 
 }
--- test/credTests.js
+++ test/credTests.js
@@ -2,7 +2,25 @@ const credController = require("../controllers/credential");
 
 describe("Credstash  testing ", function () {
 
+
+  it.skip('initCreds test', function (done) {
+    console.log(" ============================")
+    console.log(" Testing initialization of env variable MONGO_URL from credstash")
+    console.log(" ============================")
+
+    credController.initCreds().then(() => {
+      console.log(`${process.env.MONGO_URL}`);
+      return done()
+    }).catch(err => {
+      console.error(err.stack)
+      return done(err)
+    })
+  })
+
   it('get MUG mongo password', function (done) {
+    console.log(" ============================")
+    console.log(" Testing reading of the key mug.mongo.pass")
+    console.log(" ============================")
     credController.getCredential("mug.mongo.pass").then(pass => {
       console.log(pass)
       return done()
@@ -12,12 +30,5 @@ describe("Credstash  testing ", function () {
     })
   })
 
-  it('initCreds test', function (done) {
-    credController.initCreds().then(() => {
-      return done()
-    }).catch(err => {
-      console.error(err.stack)
-      return done(err)
-    })
-  })
+
 })
